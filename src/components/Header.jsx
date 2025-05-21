187d9e44e11ad16882c8ff265d7ca23d47c14311
ihssane <najiihsane22@gmail.com>
2025-05-21 13:35:18 +0100

    Add Howler.js for sound effects, implement timer functionality in Quiz, and update Header for mobile responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../assets/quiz.png"
+import MenuIcon from '@mui/icons-material/Menu';
 export const Header = () => {
+  const [isToggled,setIsToggled]= useState(false);
+  const handleToggle=()=>{
+    setIsToggled(!isToggled);
+  }
   return (
     
-   <div className='fixed top-0 bg-[#fffcfc] w-full '>
-     <nav className='flex  justify-between font-poppins '>
+   
+     <nav className='flex items-center justify-between font-poppins py-3 pr-3 fixed top-0  bg-[#fffcfc] w-full'>
       <div className='px-8 flex items-center gap-3'>
         <img src={logo} className='w-13 h-13 text-' />
         <span className='text-2xl font-bold text-gray-700'>Quiz Time</span>
       </div>
+      <div>
+     
+      </div>
         <div className=''>
-          <ul className=' flex justify-end gap-4 p-4 font-medium'>
-            <li className=''><button className='p-3 bg-blue-500  cursor-pointer text-white rounded-2xl w-24 hover:bg-blue-600'>Log In</button></li>
+           <div className='relative lg:hidden' onClick={handleToggle}>
+              <MenuIcon fontSize='large' className='' />
+          </div>
+          <ul className={` ${isToggled?"flex flex-col absolute right-0 top-16 shadow-lg  bg-[#fffcfc] w-full":"hidden"} lg:flex-row lg:flex justify-end gap-4 p-4 font-medium  `}>
+            <li className=''><button className={` ${isToggled? "bg-[#fffcfc] text-black":'bg-blue-500 text-white '} p-3  cursor-pointer  rounded-2xl w-24 hover:bg-blue-600`}>Log In</button></li>
           
             <li className=''><button className='p-3 rounded-2xl hover:underline w-24 cursor-pointer'>Sign up</button></li>
         </ul>
+
         </div>
     </nav>
-   </div>
+   
   )
 }
--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -1,22 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { getDocs, collection, db, doc } from "../services/firebase.js";
-import { Header } from "../components/Header.jsx";
 import CheckCircleOutlineOutlinedIcon from "@mui/icons-material/CheckCircleOutlineOutlined";
 import HighlightOffOutlinedIcon from "@mui/icons-material/HighlightOffOutlined";
-
+import stopWatch from "../assets/stopwatch.gif"
+import Tooltip from '@mui/material/Tooltip';
+import { Howl } from "howler";
+import beep from '../assets/sounds/beep_sound.mp3'
 const Quiz = () => {
   const { id } = useParams();
   const [questions, setQuestions] = useState([]);
   const [questionIndex, setQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
   const [isAnswered, setIsAnswered] = useState({});
+  const [timePerQuestion, setTimePerQuestion] = useState([]);
 
-  const handleNextClick = (option) => {
+
+  const handleNextClick = () => {
     setQuestionIndex((prev) => prev + 1);
+    setTimeLeft(10);
+
+    if (isTimerEnabled){
+      soundRef.current.stop();
+    };
+    setTimeLeft(10)
   };
   const handleBackClick = () => {
+     if (isTimerEnabled){
+      return
+    }
     setQuestionIndex((prev) => prev - 1);
+   
   };
 
   useEffect(() => {
@@ -57,6 +71,7 @@ const Quiz = () => {
       console.log("wrong answer");
     }
   };
+  
   const optionStyle = (option) => {
     if (questionIndex in isAnswered) {
       const selectedAnswer = isAnswered[questionIndex];
@@ -81,8 +96,70 @@ const Quiz = () => {
     setIsAnswered({});
     setQuestionIndex(0);
     setScore(0);
+    setIsTimerEnabled(false);
   };
+  //timer state
+  const [isTimerEnabled,setIsTimerEnabled]= useState(false);
+  const toggleTimer=()=>{
+    setIsTimerEnabled(true);
+  }
+ 
+  
+ const [timeLeft,setTimeLeft]= useState(10);
+  useEffect(()=>{
+    let leftTime
+    if (isTimerEnabled){
+      leftTime=setTimeout(()=>{
+        setTimeLeft(prev=>prev-1)
+      },1000)
+    }
+    return ()=>clearTimeout(leftTime)
+  },[timeLeft,isTimerEnabled])
+  useEffect(() => {
+    let timer;
+    if (questionIndex===questions.length){
+      setIsTimerEnabled(false);
+      setTimeLeft(10);
+      return;
+    }
+    if (isTimerEnabled) {
+      
+      timer = setTimeout(() => {
+        setQuestionIndex(prev => prev + 1);
+        setTimeLeft(10)
+        if (soundRef.current){
+          soundRef.current.stop();
+        }
+        setIsAnswered(prev=>({
+          ...prev,
+          [questionIndex]:null,
+        }));
+        
+        
+      }, 10000);
+       
+    }
+    return () => {clearTimeout(timer);
+      
+    };
+    
+  }, [isTimerEnabled, questionIndex]);
 
+ 
+  const formatTime=(seconds)=>{
+    const mins=Math.floor(seconds/60);
+    const secs=seconds%60;
+    return `${mins.toString().padStart(2,'0')}:${secs.toString().padStart(2,'0')}` ;
+  }
+  const soundRef= useRef(null);
+  useEffect(()=>{
+     if (timeLeft===5){
+      
+     soundRef.current = new Howl({ src: [beep] });
+    soundRef.current.play();
+     }
+  },[timeLeft])
+  
   return (
     <div className="font-poppins ">
     
@@ -119,12 +196,12 @@ const Quiz = () => {
                 </div>
               ))}
           </div>
-          <div className="flex items-center justify-center gap-8">
+          <div className="flex items-center justify-center relative lg:static gap-8">
             <span className="text-lg text-cyan-950 font-medium">
               Question {questionIndex + 1} of {questions.length}
             </span>
             <button
-              disabled={questionIndex <= 0}
+              disabled={questionIndex <= 0 |isTimerEnabled}
               className="p-3 disabled:opacity-25 disabled:cursor-not-allowed not-disabled:cursor-pointer text-lg hover:bg-cyan-500 text-white bg-cyan-600 transition-colors duration-400 ease-in-out w-28 rounded-2xl"
               onClick={handleBackClick}
             >
@@ -136,7 +213,23 @@ const Quiz = () => {
             >
               next
             </button>
+             {isTimerEnabled&&<span className={` ${timeLeft<=5&&"text-red-500"} text-2xl text-gray-800 font-medium`}>
+              {formatTime(timeLeft)}
+            </span>}
+            
+            <div className="absolute  lg:right-4 lg:bottom-5 top-20 lg:top-17 left-0 h-15">
+               <div className="flex items-center justify-center ">
+                 <Tooltip title={<h1 style={{  fontSize: "15px" }}>Click to enable a 10 seconds timer</h1>} placement="top" arrow >
+                    <img src={stopWatch} className="lg:h-15 lg:w-15 w-20 h-20 cursor-pointer" onClick={toggleTimer} />
+                    
+                </Tooltip>
+                <span className='lg:hidden text-lg text-cyan-950 font-bold '>Click to Enable 10 seconds timer</span>
+
+               </div>
+                
+            </div>
           </div>
+
         </div>
       ) : (
         <div className="bg-cyan-600 flex flex-col gap-6 py-20 rounded-2xl justify-center  items-center p-4 mx-auto my-8 max-w-lg">
