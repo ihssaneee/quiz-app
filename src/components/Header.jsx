afdeecdb1e5f1936e0f3e1a682ce9a50f3581236
ihssane <najiihsane22@gmail.com>
2025-05-31 19:15:16 +0100

    Adds user dropdown menu with settings and logout options
    Introduces a dropdown menu in the header for authenticated users, displaying a profile picture and username. Adds functionality for toggling the dropdown visibility and includes links to settings and logout actions. Improves user experience by providing personalized navigation options.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,25 @@ import React, { useState } from 'react'
 import logo from "../assets/quiz.png"
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
+import {useAuth} from "../contexts/authContext.jsx"
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 export const Header = () => {
   const [isToggled,setIsToggled]= useState(false);
+  const [isDropdwonToggled,setIsDropdownToggled]=useState(false);
+  const {user}=useAuth();
   const handleToggle=()=>{
     setIsToggled(!isToggled);
   }
+  const handleDropDownToggle=()=>{
+    setIsDropdownToggled(!isDropdwonToggled);
+  }
+  const userImage=user?.photoURL 
   return (
     
    
-     <nav className='flex items-center justify-between font-poppins py-3 pr-3 fixed top-0  bg-[#fffcfc] w-full'>
+     <nav className='flex items-center justify-between font-poppins py-3 pr-3 fixed top-0  bg-[#fffffc] w-full'>
       <Link to="/" className='px-8 flex items-center gap-3'>
         <img src={logo} className='w-13 h-13 text-' />
         <span className='text-2xl font-bold text-gray-700'>Quiz Time</span>
@@ -18,7 +28,24 @@ export const Header = () => {
       <div>
      
       </div>
-        <div className=''>
+        {user?(
+          <div className='flex gap-2 items-center relative cursor-pointer ' onClick={handleDropDownToggle} >
+                  <div className='flex gap-2 items-center'>
+                    <img src={userImage} alt='profile picture' className='w-10 h-10 rounded-full' />
+                 <p className="text-lg font-semibold text-gray-800   ">{user.displayName }</p>
+                  <KeyboardArrowDownIcon fontSize='large' className='text-gray-600'  />
+                  </div>
+                
+                 <div className={`absolute top-13 flex flex-col gap-3  right-0 py-6 rounded-2xl  border-neutral-100 border   shadow-lg w-full bg-[#fffffc] ${isDropdwonToggled?"block":"hidden"} `}>
+                <Link to="" className='text-lg flex gap-2 items-center px-6 py-2 text-gray-700 hover:bg-gray-50'><SettingsOutlinedIcon fontSize='medium' className='text-gray-800' />Settings</Link>
+                <button  className='text-lg flex gap-2 items-center px-6 cursor-pointer hover:bg-gray-50 py-2 text-gray-700'><LogoutOutlinedIcon fontSize='medium' className='text-gray-800' />Log out</button>
+             
+            </div>
+             
+
+          </div>
+         
+        ):<div className=''>
            <div className='relative lg:hidden' onClick={handleToggle}>
               <MenuIcon fontSize='large' className='' />
           </div>
@@ -28,7 +55,7 @@ export const Header = () => {
             <li className=''><Link to="/signUp" className='p-3 rounded-2xl hover:underline w-24 cursor-pointer'>Sign up</Link></li>
         </ul>
 
-        </div>
+        </div>}
     </nav>
    
   )
